7b011e9175648f43fe4bcdf67ef2a6958482d219
arshavinod14 <arshavinod41@gmail.com>
2024-06-27 16:54:39 +0530

    update settings.py

--- backend/travel/travel/settings.py
+++ backend/travel/travel/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ["*", "travelmate-frontend.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
